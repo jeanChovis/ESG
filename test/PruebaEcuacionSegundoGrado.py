78d6413fe2b71e4e2b4874d61b55f14573d4cbb6
jean <76266001@continental.edu.pe>
2023-11-15 13:16:21 -0500

    Se realizo el test para las Excepciones

--- test/PruebaEcuacionSegundoGrado.py
+++ test/PruebaEcuacionSegundoGrado.py
@@ -40,6 +40,15 @@ class PruebaEcuacionSegundoGrado(unittest.TestCase):
         self.assertEqual(RaizEsperada1, RaizActual1)
         self.assertEqual(RaizEsperada2, RaizActual2)
 
+    def test_solucionESG_parametrosNoNumericos_lanzaException(self):
+        # Arrange
+        ecuacionSegundoGrado = EcuacionSegundoGrado()
+
+        # Assert
+        self.assertTrue(True)
+        with self.assertRaises(ValueError):
+            ecuacionSegundoGrado.definirParametros("3.1", "5.2", "c")
+
 
     def test_solucionESG_parametrosNumericos_raicesReales_subTest(self):
         # Arrange
@@ -84,3 +93,19 @@ class PruebaEcuacionSegundoGrado(unittest.TestCase):
             # Assert
             self.assertEqual(item["RaizEsperada1"], RaizActual1)
             self.assertEqual(item["RaizEsperada2"], RaizActual2)
+
+    def test_solucionESG_parametrosNoNumericos_lanzaException_subTest(self):
+        ecuacionSegundoGrado = EcuacionSegundoGrado()
+
+        items = (
+            {"Case": "Caso 01", "a": "a", "b": "b", "c": "c"},
+            {"Case": "Caso 02", "a": "a", "b": 1, "c": 1},
+            {"Case": "Caso 03", "a": 1, "b": "aa", "c": 1},
+            {"Case": "Caso 03", "a": 1, "b": "3,1", "c": 1},
+        )
+
+        for item in items:
+            with self.subTest(item["Case"]):
+                self.assertTrue(True)
+                with self.assertRaises(ValueError):
+                    ecuacionSegundoGrado.definirParametros(item["a"], item["b"], item["c"])
